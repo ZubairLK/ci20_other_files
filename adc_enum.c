a3a7f012c7be0c48f78899312a10626998c3d23c
Zubair Lutfullah Kakakhel <Zubair.Kakakhel@imgtec.com>
2014-10-20 16:32:06 +0100

    Add a tiny C file to check which enum number it becomes.
    There seem to be hidden undocumented interrupt lines.
    3.0.8 code points to these numbers for ADC.. :s
    
    Lets see

---
+++ adc_enum.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define INTC_IRQ_BASE		8
+
+enum {
+#define INTC_NR_IRQS	64
+	IRQ_INTC_BASE = INTC_IRQ_BASE,
+	IRQ_INTC_END = IRQ_INTC_BASE + INTC_NR_IRQS - 1,
+
+#define OST_NR_IRQS	1
+	IRQ_OST_BASE,
+	IRQ_OST_END = IRQ_OST_BASE + OST_NR_IRQS - 1,
+
+#define GPIO_NR_IRQS	(32*6)
+	IRQ_GPIO_BASE,
+	IRQ_GPIO_END = IRQ_GPIO_BASE + GPIO_NR_IRQS - 1,
+
+#define SADC_NR_IRQS	(6)
+	IRQ_SADC_BASE,
+	IRQ_SADC_END = IRQ_SADC_BASE + SADC_NR_IRQS - 1,
+
+#define MCU_NR_IRQS	(5)
+	IRQ_MCU_BASE,
+	IRQ_MCU_END = IRQ_MCU_BASE + MCU_NR_IRQS - 1,
+
+#define RESERVED_NR_IRQS	(100)
+	IRQ_RESERVED_BASE,
+	IRQ_RESERVED_END = IRQ_RESERVED_BASE + RESERVED_NR_IRQS - 1,
+	NR_IRQS,
+};
+
+void main ()
+
+{
+	printf("IRQ is number %d\n" , IRQ_SADC_BASE);
+}
